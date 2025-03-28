2e26e03b85d8b1a6702b0a2ba5c7042f85c61e61
Ruben <r.bojorquez@cyberpuerta.mx>
2025-03-28 15:59:06 -0700

    chore: Fetch data with useAsyncData -

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+const route = useRoute();
+const config = useRuntimeConfig();
+
+const { data } = await useAsyncData(() => {
+  return $fetch(`http://www.omdbapi.com/?apikey=${config.public.apiKey}&i=${route.params.id}`);
+});
+</script>
+
 <template>
   <div>
-    <p>Movie ID: {{ $route.params.id }}</p>
+    <pre>{{ data }}</pre>
   </div>
 </template>
 

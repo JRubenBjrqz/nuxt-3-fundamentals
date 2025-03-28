b759f289badcff7668e724165e406fe2d9232c5e
Ruben <r.bojorquez@cyberpuerta.mx>
2025-03-28 16:27:29 -0700

    chore: Configure useAsyncData with Options -

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -4,6 +4,9 @@ const config = useRuntimeConfig();
 
 const { data } = await useAsyncData(`/movies/${route.params.id}`, () => {
   return $fetch(`http://www.omdbapi.com/?apikey=${config.public.apiKey}&i=${route.params.id}`);
+}, 
+{
+  pick: ['Plot', 'Title'],
 });
 </script>
 

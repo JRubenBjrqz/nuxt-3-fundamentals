65c4b2ca544d88a2be3beade089bc47d76b9d58b
Ruben <r.bojorquez@cyberpuerta.mx>
2025-03-28 15:30:40 -0700

    chore: Fetch Data with  -

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-})
+  runtimeConfig: {
+    public: {
+      apiKey: process.env.API_KEY,
+    },
+  },
+});
--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const query = ref('');
+const movies = ref([]);
+const config = useRuntimeConfig();
+
+async function search() {
+  const { Search } = await $fetch(`http://www.omdbapi.com/?apikey=${config.public.apiKey}&s=${query.value}`);
+  movies.value = Search;
+}
+</script>
 
 <template>
-  <div>
-    Page: movies
-  </div>
+  <form @submit.prevent="search">
+    <input type="text" v-model="query"/>
+    <button type="submit">Search</button>
+  </form>
+  <ul style="display: flex; flex-wrap: wrap; gap: 10px; list-style: none;">
+    <li v-for="movie in movies" :key="movie.imdbID">
+      <NuxtLink :to="{ name: 'movies-id', params: { id: movie.imdbID } }">
+        <img :src="movie.Poster" :alt="movie.Title" />
+      </NuxtLink>
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>

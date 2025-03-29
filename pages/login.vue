0c260f1bad504f0c386509cc37efd9e6abe59085
Ruben <r.bojorquez@cyberpuerta.mx>
2025-03-28 17:51:04 -0700

    chore: Create Flexible Custom Layouts -

---
+++ pages/login.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'plain',
+});
+</script>
+
+<template>
+  <form @submit.prevent="login">
+    <h1>Login</h1>
+    <label>
+      Username
+      <input type="text" />
+    </label>
+    <label>
+      Password
+      <input type="password" />
+    </label>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<style scoped></style>

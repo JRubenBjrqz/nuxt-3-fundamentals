7f29f23eb2c8963cdb5d272cd85343fcc0542703
Ruben <r.bojorquez@cyberpuerta.mx>
2025-03-28 17:01:34 -0700

    chore: Handle useFetch Errors -

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -3,9 +3,13 @@ const route = useRoute();
 const config = useRuntimeConfig();
 
 const { data } = useFetch(`http://www.omdbapi.com/?apikey=${config.public.apiKey}&i=${route.params.id}`, {
-  pick: ['Plot', 'Title'],
+  pick: ['Plot', 'Title', 'Error'],
   key: `movie-${route.params.id}`,
 });
+
+if (data.value.Error === 'Incorrect IMDb ID.') {
+  showError({ statusCode: 404, statusMessage: 'Page not found' });
+}
 </script>
 
 <template>
